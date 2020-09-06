e93a1d243646b692b86153430815243564468030
JulianLin549 <sres3416@gmail.com>
2020-09-06 18:04:22 +0800

    Updates

---
+++ precache-manifest.44866723cf622bc20938a6e9f475a1a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583a0b6d6b06944b27d05af9e0979fcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d9cf5184663aa450b493",
+    "url": "/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9cf5184663aa450b493",
+    "url": "/static/js/main.3ac52779.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "3afbef6ed2e7c045575d9f4131e37095",
+    "url": "/static/media/resume.3afbef6e.pdf"
+  }
+]);
